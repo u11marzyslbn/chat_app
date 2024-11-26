4e0770b5c4ecc0e05d4685ca8649dd57a5592528
Marcin Żyszkiewicz Techni <u11_marzys_lbn@technischools.com>
2024-11-26 10:45:45 +0000

    Inicjalized chat app

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,24 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static CHAT: RefCell<Vec<String>> = RefCell::new(Vec::new());
+}
+
+#[ic_cdk::update]
+fn save_chat(input_chat: String) {
+    CHAT.with(|chat| chat.borrow_mut().push(input_chat))
+}
+
+#[ic_cdk::query]
+fn get_chat() -> Vec<String> {
+    CHAT.with(|chat| chat.borrow().clone())
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
+
+// fn moja_nowa_funkcja() {
+//     print!("Hello, World!");
+// }
--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -6,10 +6,8 @@ let greeting = ref('');
 async function handleSubmit(e) {
   e.preventDefault();
   const target = e.target;
-  const name = target.querySelector('#name').value;
-  await my_project_backend.greet(name).then((response) => {
-    greeting.value = response;
-  });
+  const chat = target.querySelector('#chat').value;
+  await my_project_backend.save_chat(chat);
 }
 </script>
 
@@ -19,8 +17,8 @@ async function handleSubmit(e) {
     <br />
     <br />
     <form action="#" @submit="handleSubmit">
-      <label for="name">Enter your name: &nbsp;</label>
-      <input id="name" alt="Name" type="text" />
+      <label for="chat">Enter your message: &nbsp;</label>
+      <input id="chat" alt="chat" type="text" />
       <button type="submit">Click Me!</button>
     </form>
     <section id="greeting">{{ greeting }}</section>
